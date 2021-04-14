be76dce24323e209ab390ea0479072735a6ade15
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-14 11:36:28 +0900

    カウンター作成

---
+++ pages/countComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <p>state</p>
+    {{ $store.state.count }}
+    <button @click="$store.dispatch('upCountAction')">countUp</button>
+
+    <p>this</p>
+    {{ count }}
+    <button @click="countup">数値アップ</button>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      count: 0
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    countup(){
+      this.count += 1
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
